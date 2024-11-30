4628d2bb71cba36ed62057353494f2fb7f6488c1
DENYS <132446947+hyppolit@users.noreply.github.com>
2024-11-30 17:58:13 +0100

    projet html

---
+++ styles.js
@@ -0,0 +1,4 @@
+var bouton = document.getElementById("alerteBtn");
+bouton.onclick = function() {
+    alert("Monsieur ne vous intquetter pas je ne suis pas macho ce site est aussi pour les femmes");
+};
